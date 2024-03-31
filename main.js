a7d9b72dcb50e2e6041398f950fdf0d3c0b79785
nimra <nimra271430@gmail.com>
2024-03-31 11:32:54 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var Name = "Nimra";
+console.log(Name);
+var age = 19;
+console.log(age);
+var height = 5.2;
+console.log(height);
+var response = "true";
+console.log(response);
---
+++ main.ts
@@ -0,0 +1,8 @@
+let Name = "Nimra"; 
+console.log(Name);
+let age = 19;
+console.log(age);
+const height = 5.2;
+console.log (height);
+let response = "true";
+console.log(response);
